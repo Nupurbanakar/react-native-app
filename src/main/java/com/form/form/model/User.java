4a723d272a2a3c2a868af85005aac74a4ef547fd
Nupurbanakar <nupurbanakar@gmail.com>
2024-10-25 11:08:29 +0530

    Updated ID generation strategy to use GenerationType.IDENTITY

--- src/main/java/com/form/form/model/User.java
+++ src/main/java/com/form/form/model/User.java
@@ -1,15 +1,16 @@
 package com.form.form.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.Generated;
 
 @Entity
 @Data
 public class User {
     @Id
-    @Generated
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
